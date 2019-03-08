0592460609b661cb47ada304e2099c9db1c14943
Sean Collins <sgc29@zips.uakron.edu>
2019-03-07 22:40:19 -0500

    Fix typo

--- prepare_to_train.py
+++ prepare_to_train.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
 
 	# Create TFRecords
 	os.system("""
-		python,
-		utilities/create_tf_record.py,
-		--data_dir=data,
-		--training_set_output_path=data/training_set.tfrecord,
-		--eval_set_output_path=data/eval_set.tfrecord,
-		--num_examples=300,
-		--training_ratio=1,
-		--label_map_path=models/label_map.pbtxt
+		python
+		utilities/create_tf_record.py
+		--data_dir=data
+		--training_set_output_path=data/training_set.tfrecord
+		--eval_set_output_path=data/eval_set.tfrecord
+		--num_examples=300
+		--training_ratio=1
+		--label_map_path=models/label_map.pbtx
 	""")
