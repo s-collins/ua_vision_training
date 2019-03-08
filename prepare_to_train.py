8a0232ecd6469562dfc7b08f7dc31175556bc56f
Sean Collins <sgc29@zips.uakron.edu>
2019-03-07 22:37:31 -0500

    Add script to prepare for training

---
+++ prepare_to_train.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+if __name__ == '__main__':
+	
+	# Create TFRecords
+	subprocess.run([
+		"python",
+		"utilities/create_tf_record.py",
+		"--data_dir=data",
+		"--training_set_output_path=data/training_set.tfrecord",
+		"--eval_set_output_path=data/eval_set.tfrecord",
+		"--num_examples=300",
+		"--training_ratio=1",
+		"--label_map_path=models/label_map.pbtxt"
+	])
