d78b594bf2eeaab3b2d6153425e92be8769a8a23
Sean Collins <sgc29@zips.uakron.edu>
2019-03-08 17:25:16 -0500

    Fixed save script

--- save_trained_model.py
+++ save_trained_model.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 	subprocess.call([
 		'python',
 		'/home/paperspace/models/research/object_detection/export_inference_graph.py',
-		'--pipeline_config_path=output/configuration.config',
-		'--trained_checkpoint_prefix=output/model.ckpt',
+		'--pipeline_config_path=output/pipeline.config',
+		'--trained_checkpoint_prefix=output/model.ckpt-5000',
 		'--output_directory=output'
 	])
