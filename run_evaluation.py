622c4e318b40f0aa4d7d51379879459e749cda30
Sean Collins <sgc29@zips.uakron.edu>
2019-04-24 17:01:10 -0400

    Fix bug in evaluation configuration

--- run_evaluation.py
+++ run_evaluation.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
         'python',
         '/home/paperspace/models/research/object_detection/legacy/eval.py',
         '--logtostderr',
-        '--pipeline_config_path=output/configuration.config',
+        '--pipeline_config_path=models/ssd_mobilenet_v1_coco/configuration.config',
         '--checkpoint_dir=output',
         '--eval_dir=eval'
     ])
